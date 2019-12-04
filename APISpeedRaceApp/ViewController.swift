69c293fff435516779148d507c5775143d23afa8
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-04 22:36:46 +0900

    ViewControllerのUI部品とコードの連携、実装する内容をコメントで記載

--- APISpeedRaceApp/ViewController.swift
+++ APISpeedRaceApp/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var waitingAPI1: UILabel!
+    @IBOutlet weak var waitingAPI2: UILabel!
+    @IBOutlet weak var waitingAPI3: UILabel!
+    
+    @IBOutlet weak var goNextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        //API通信が完了してからボタンを表示
+        goNextButton.isHidden = true
     }
-
+    
+    
+    //API通信を開始
+    @IBAction func startCommunication(_ sender: Any) {
+        
+        //API通信を順番通りに行う
+        
+        //終了後にラベルの名称を"通信完了"と変更
+    }
+    
+    
+    //次の画面へ遷移
+    @IBAction func goNext(_ sender: Any) {
+        performSegue(withIdentifier: "next", sender: nil)
+    }
+    
+    
 
 }
 
