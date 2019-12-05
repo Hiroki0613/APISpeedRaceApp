1cc61c5791a4148b34c063671ae6656b2018a775
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-05 22:31:32 +0900

    GurunaviAPIを通信１として実装

--- APISpeedRaceApp/ViewController.swift
+++ APISpeedRaceApp/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -30,8 +32,75 @@ class ViewController: UIViewController {
     @IBAction func startCommunication(_ sender: Any) {
         
         //API通信を順番通りに行う
-        
-        //終了後にラベルの名称を"通信完了"と変更
+        //大業態のカテゴリー取得
+                DispatchQueue.global().async {
+                    
+                    DispatchQueue.main.async {
+                        
+                        let urlBigClassificationString = "https://api.gnavi.co.jp/master/CategoryLargeSearchAPI/v3/?keyid=2f4c7acc59914e1efa7bc6eef258308f"
+                        
+                        let encordeUrlString:String = urlBigClassificationString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                        
+                        Alamofire.request(encordeUrlString, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+                            
+                            
+                            
+                            //大業態で現れるコードをvar bigCategoryで取得
+                            switch response .result {
+                            case .success(let happy):
+                                let json:JSON = JSON(response.data as Any)
+                                
+                            print("大業態への通信成功")
+                                
+                            case .failure(let error):
+                                print(error)
+                            }
+                        }
+                    }
+                    //同期処理終了
+                }
+                //非同期処理終了
+                
+                //カテゴリーでの検索結果を表示
+                DispatchQueue.global().async {
+                    DispatchQueue.main.async {
+                        
+                        //let urlBigClassificationString = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=2f4c7acc59914e1efa7bc6eef258308f&hit_per_page=20&freeword=\(keyword)&category_l=\(self.bigCategoryCodeString)"
+                        
+                        let urlBigClassificationString = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=2f4c7acc59914e1efa7bc6eef258308f&hit_per_page=20&category_l=RSFST01000"
+                                      
+                        
+                        let encordeUrlString:String = urlBigClassificationString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                        
+                        Alamofire.request(encordeUrlString, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON(completionHandler: { (secondResponce) in
+                            
+                            //print("hirohiro2")
+                            //print(secondResponce)
+                            
+                            switch secondResponce .result {
+                            case .success(let happy):
+                                
+                                for i in 0...10{
+                                    let json:JSON = JSON(secondResponce.data as Any)
+                                    let shopName = json["rest"][i]["name"].string
+                                    let shopImage = json["rest"][i]["image_url"]["shop_image1"].string
+                                    let shopAddress = json["rest"][i]["address"].string
+                                    let latitude = json["rest"][i]["latitude"].string
+                                    let longitude = json["rest"][i]["longitude"].string
+                                    
+                                    
+                                }
+                                
+                                self.waitingAPI1.text = "通信完了1"
+                                
+                                
+                            case .failure(let error):
+                                print(error)
+                            }
+                        })
+                        
+                    }
+                }
     }
     
     
